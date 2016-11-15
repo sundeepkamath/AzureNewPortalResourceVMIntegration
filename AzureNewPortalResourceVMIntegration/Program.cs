7628af365b97234620b7187cd1689acfa45c35b2
sundeep kamath <sundeep.g.kamath@gmail.com>
2016-11-15 11:29:55 +0530

    Initial Commit

--- AzureNewPortalResourceVMIntegration/Program.cs
+++ AzureNewPortalResourceVMIntegration/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AzureNewPortalResourceVMIntegration
 {
@@ -10,6 +6,7 @@ namespace AzureNewPortalResourceVMIntegration
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!!");
         }
     }
 }
