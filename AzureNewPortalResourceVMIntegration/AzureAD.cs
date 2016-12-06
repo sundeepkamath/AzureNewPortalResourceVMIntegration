5fd5d45f3547ee13a867a285f52ef056aa570727
sundeep kamath <sundeep.g.kamath@gmail.com>
2016-12-06 10:27:04 +0530

    Updated solution

---
+++ AzureNewPortalResourceVMIntegration/AzureAD.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System;
+
+namespace AzureNewPortalResourceVMIntegration
+{
+    public class AzureAD
+    {
+        internal static string Authenticate(string authority, 
+            string serviceResourceId, string clientId, string secretKey)
+        {
+            AuthenticationContext authContext = new AuthenticationContext(authority, true);
+            AuthenticationResult authResult = null;
+
+            try
+            {
+                authResult = authContext
+                    .AcquireTokenAsync(serviceResourceId, 
+                                new ClientCredential(clientId, secretKey)).Result;
+
+                if (authResult != null)
+                    Console.WriteLine("Auth Token: {0}", authResult.AccessToken);
+                else
+                    throw new Exception("Azure AD authentication failed.");
+
+            }
+            catch (AdalException adEx)
+            {
+                Console.WriteLine("Exception: {0}", adEx);
+            }
+
+            return authResult.AccessToken;
+        }
+    }
+}
--- AzureNewPortalResourceVMIntegration/Program.cs
+++ AzureNewPortalResourceVMIntegration/Program.cs
@@ -6,7 +6,33 @@ namespace AzureNewPortalResourceVMIntegration
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!! ");
+            
+            string tenantId = "<Enter Directory Id>";
+            string clientId = "<Enter Application Id>";
+            string secretKey = "<Enter Secret Key>";
+            string subscriptionId = "<Enter Subscription Id>";
+            string resourceGroupName = "<Enter Resource group name>";
+            string virtualMachineName = "<Enter Virtual Machine name>";
+
+            string oauthUrl = "https://login.microsoftonline.com/{0}/oauth2/authorize";
+            string serviceResourceId = "https://management.core.windows.net/";
+
+            string authority = string.Format(oauthUrl, tenantId);
+
+            string vmDetailsURL = $"https://management.azure.com/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Compute/virtualMachines/{virtualMachineName}?api-version=2015-06-15";
+
+            string accessToken = AzureAD.Authenticate(authority, serviceResourceId, clientId, secretKey);
+
+            if (accessToken != null)
+            {
+                AzureRestClient.ExecuteRequest(accessToken, vmDetailsURL)
+                     .ContinueWith((task) =>
+                     {
+                         Console.WriteLine(task.Result);
+                     });
+
+            }
+            Console.ReadLine();
         }
     }
 }
